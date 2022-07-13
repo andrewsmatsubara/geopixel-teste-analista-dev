a3e25a2e08adc69538adee1d5cdaab1d8be5fd12
Andrews Matsubara <andrewsmatsubara@gmail.com>
2022-07-13 11:35:32 -0300

    Clock component and CSS finished

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from 'react';
 import '../style/Clock.css';
 
 export const Clock = () => {
+  const [time, setTime] = useState('');
+
+  const getTime = () => {
+    const date = new Date();
+    const current = date.toLocaleTimeString();
+
+    setTime(current);
+  }
+
+  useEffect(() => {
+    setTimeout(getTime, 1000);
+  });
+
   return (
-    <div className='clock-container'>Clock</div>
+    <div className='clock-container'>{time}</div>
   )
 }
