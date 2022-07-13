c85a5a4d1f750f3da965ab9a8c24c58666d5cae9
Andrews Matsubara <andrewsmatsubara@gmail.com>
2022-07-13 13:20:34 -0300

    ItemHandler component construction with handleChange function and state management for input values

--- src/components/ItemHandler.jsx
+++ src/components/ItemHandler.jsx
@@ -1,7 +1,28 @@
+import { useState } from 'react';
 import '../style/ItemHandler.css';
 
 export const ItemHandler = () => {
+  const [item, setItem] = useState('');
+
+  const defaultValue = 'Selecione uma opção';
+
+  const handleChange = (e) => {
+    setItem(e.target.value);
+  }
+
   return (
-    <div className='item-handler-container'>Item Handler</div>
+    <form className='item-handler-container' onSubmit={(e) => e.preventDefault()}>
+      <select defaultValue={defaultValue}>
+        <option value={defaultValue} disabled>{defaultValue}</option>
+        <option>Opção 1</option>
+      </select>
+      <input placeholder='Insira um item' onChange={(e) => handleChange(e)} />
+      <div className='button-container'>
+        <button>Adicionar</button>
+        <button>Remover</button>
+        <button>Limpar caixa</button>
+      </div>
+      <p></p>
+    </form>
   )
 }
