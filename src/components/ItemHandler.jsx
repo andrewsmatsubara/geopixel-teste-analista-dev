9f72ea49f77c0104bbc17388ddd359f44a7f84f9
Andrews Matsubara <andrewsmatsubara@gmail.com>
2022-07-13 18:32:13 -0300

    Implemented add button

--- src/components/ItemHandler.jsx
+++ src/components/ItemHandler.jsx
@@ -1,24 +1,52 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import '../style/ItemHandler.css';
 
 export const ItemHandler = () => {
   const [item, setItem] = useState('');
+  const [select, setSelect] = useState('');
+  const [aux, setAux] = useState(false);
 
   const defaultValue = 'Selecione uma opção';
+
+  const handleClick = async () => {
+    setSelect(item);
+
+    if (aux === false) {
+      setAux(true);
+    } else if (aux === true) {
+      setAux(false);
+    }
+  }
 
   const handleChange = (e) => {
     setItem(e.target.value);
   }
 
+  const createElement = (i) => {
+    const selected = document.querySelector('.select');
+    const option = document.createElement('option');
+
+    option.innerHTML = i;
+
+    selected.appendChild(option);
+  }
+
+  useEffect(() => {
+    if (aux) {
+      createElement(select);
+      setAux(false);
+    }
+  }, [aux]);
+
   return (
     <form className='item-handler-container' onSubmit={(e) => e.preventDefault()}>
-      <select defaultValue={defaultValue}>
+      <select defaultValue={defaultValue} className='select'>
         <option value={defaultValue} disabled>{defaultValue}</option>
         <option>Opção 1</option>
       </select>
-      <input placeholder='Insira um item' onChange={(e) => handleChange(e)} />
+      <input placeholder='Insira um item' value={item} onChange={(e) => handleChange(e)} />
       <div className='button-container'>
-        <button>Adicionar</button>
+        <button onClick={() => handleClick()}>Adicionar</button>
         <button>Remover</button>
         <button>Limpar caixa</button>
       </div>
