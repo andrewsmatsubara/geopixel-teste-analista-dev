8cbc6fe8104ab6fa64ee35eff5443ce94795ed34
Andrews Matsubara <andrewsmatsubara@gmail.com>
2022-07-14 17:55:53 -0300

    Getting width and height values, randomizing two numbers according to those values and setting new coordinates for the circle(no working yet)

--- src/components/ShyBall.jsx
+++ src/components/ShyBall.jsx
@@ -1,12 +1,27 @@
+import { useState } from 'react';
 import '../style/ShyBall.css';
 
 export const ShyBall = () => {
-  const fleeRandomly = () => {
+  const [coordinates, setCoordinates] = useState({});
+
+  const fleeRandomly = async () => {
+    const div = document.querySelector('.shy-ball-container');
     const circle = document.querySelector('.circle');
+    const width = window.getComputedStyle(div).width.slice(0, -2);
+    const height = window.getComputedStyle(div).height.slice(0, -2);
+    const randomNumbers = await randomizeNumbers(width, height);
 
-    // circle.style.transform.translateY('200px');
+    setCoordinates(randomNumbers);
 
-    console.log(circle.style.transform);
+    circle.style.left = coordinates.x;
+    circle.style.top = coordinates.y;
+  }
+
+  const randomizeNumbers = async (maxH, maxV) => {
+    const horizontal = Math.round(Math.random() * maxH) * (Math.round(Math.random()) ? 1 : -1);
+    const vertical = Math.round(Math.random() * maxV) * (Math.round(Math.random()) ? 1 : -1);;
+
+    return { x: horizontal, y: vertical }
   }
 
   return (
